38ef70093d41c0e9136c42b65a2856b4061b3f44
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-21 21:57:59 +0100

    Init commit

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+  *
+  *
+  *
+  */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0, i;
+	int *count_ptr = &count;
+
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			/* check if the next char is format specifier*/
+			switch (format[i + 1])
+			{
+				case 'c':
+					print_char(args);
+					count++;
+					break;
+				case 's':
+					/* use ptr for count */
+					print_str(args, count_ptr);
+					break;
+				default:
+					return (count);
+			}
+		}
+		else
+		{
+			/* write the char to stdout */
+			write(1, format[i], 1);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+  * print_char: prints a char to stdout
+  * @args: a variable list of arguments
+  * to be increased after printing a character
+  * Return: void
+  */
+void print_char(va_list args)
+{
+	
+}
+
+/**
+  * print_str: prints a string to stdout
+  * @args: a variable list of arguments
+  * @count_ptr: a pointer to the character count
+  * to be increased after printing a character
+  * Return: void
+  */
+void print_str(va_list args, int *count_ptr)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+void print_char(va_list args);
+void print_str(va_list args, int *count_ptr);
+#endif
