d0b63d5e78c6128862cf5f66a62a5acb3f61671c
adam <adam@.gmail.com>
2023-04-22 12:54:04 -0700

    Add prototype for new function
    	-Add prototype for convert_num_str function.

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 void print_char(va_list args);
-void print_str(va_list args, int *count_ptr);
+void print_str(char *str, int *count_ptr);
+void convert_num_str(int num, char *str);
+
 #endif
