4e2bfd3128b5113e6a08751c1c9c3f7f2dcec4c9
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-22 12:51:56 +0100

    add function to convert int to string

--- main.c
+++ main.c
@@ -70,7 +70,31 @@ void print_char(va_list args)
   */
 void print_str(va_list args, int *count_ptr)
 {
+}
 
+/**
+ * convert_num_to_str - converts a number to a string
+ * @num: the number to be converted
+ * @str: the string to store the converted number
+ * Return: nothing
+ */
+void convert_num_to_str(int num, char *str)
+{
+	int i, rem, len = 0, n;
+
+	n = num;
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	for (i = 0; i < len; i++)
+	{
+		rem = num % 10;
+		num = num / 10;
+		str[len - (i + 1)] = rem + '0';
+	}
+	str[len] = '\0';
 }
 
 int main(void)
