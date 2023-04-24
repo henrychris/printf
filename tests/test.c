f086d2b720103a07ad53858226326e12be0d8002
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-24 07:46:16 +0100

    removed count_ptr and upgraded test suite

--- tests/test.c
+++ tests/test.c
@@ -3,12 +3,48 @@
 /**
  * main - Entry point
  * Return: Always 0
-*/
+ */
 int main(void)
 {
-	_printf(NULL);
-	_printf("%s %s %c%c%% %i %i %i %i %i %b ", "Hello", "Hello", 'a', 'b', 0, 3, -8439392, INT_MAX, -2147483647, 10);
+	int len, len1, len2, len_int;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+
+	printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+
+	_printf("C%%%c\n", 'a');
+	printf("C%%%c\n", 'a');
+
+	len = _printf("Character:[%s]\n", "Him");
+	len2 = printf("Character:[%s]\n", "Him");
+
+	printf("String length: %d\nString length 2: %d\n", len, len2);
+
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+
+	len1 = _printf("Len:[%]\n", len);
 	printf("\n");
-	printf("%s %s %c%c%% %i %i %i %i %i %i ", "Hello", "Hello", 'a', 'b', 0, 3, -8439392, INT_MAX, -2147483647, 10);
+	len2 = _printf("Len2:[%]\n", len2);
+	printf("\n");
+
+	len1 = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+
+	len_int = printf("Len:[%d]\n", 10000);
+	printf("Len:[%d]\n", len_int);
+
+	printf("%d\n%d\n", len1, len2);
+
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+
+	_printf("Wrong: %c\n");
+	printf("Wrong: %c\n");
+	
+	_printf("Null: %s\n", NULL);
+	printf("Null: %s\n", NULL);
 	return (0);
 }
