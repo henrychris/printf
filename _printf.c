843a33b23560d077f1cce7b6b5f7ef89d9c7ee1c
adam <adam@.gmail.com>
2023-04-24 07:59:13 -0700

    Add define 	To reduce the number of lines

--- _printf.c
+++ _printf.c
@@ -7,42 +7,44 @@
  * @...: arguments to be printed
  * Return: void
  */
+#define FORLOOP \
+	for (i = 0; format && format[i] != '\0'; i++) \
+	{ \
+		if (format[i] != '%') \
+		{ \
+			count += print_char(format[i]); \
+			continue; \
+		} \
+		switch (format[i + 1]) \
+		{ \
+		case 'c': \
+			count += print_char(va_arg(args, int)); \
+			break; \
+		case 's': \
+			count += print_str(va_arg(args, char *)); \
+			break; \
+		case '%': \
+			count += print_char('%'); \
+			break; \
+		case 'i': \
+		case 'd': \
+			count += print_int(va_arg(args, int)); \
+			break; \
+		case 'b': \
+			count += print_binary(va_arg(args, int)); \
+			break; \
+		default: \
+			return (-1); \
+		} \
+		i++; \
+	}
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0, i;
 
 	va_start(args, format);
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			count += print_char(format[i]);
-			continue;
-		}
-		switch (format[i + 1])
-		{
-		case 'c':
-			count += print_char(va_arg(args, int));
-			break;
-		case 's':
-			count += print_str(va_arg(args, char *));
-			break;
-		case '%':
-			count += print_char('%');
-			break;
-		case 'i':
-		case 'd':
-			count += print_int(va_arg(args, int));
-			break;
-		case 'b':
-			count += print_binary(va_arg(args, int));
-			break;
-		default:
-			return (-1);
-		}
-		i++;
-	}
+	FORLOOP
 	va_end(args);
 	return ((format) ? count : -1);
 }
